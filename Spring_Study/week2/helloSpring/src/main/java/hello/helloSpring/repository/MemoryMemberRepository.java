d0ff904a22b5a9a7582ebadb3c87ba827453f63a
hyunmin0809 <khm010809@naver.com>
2024-10-05 23:18:17 +0900

    add: week2 code

---
+++ Spring_Study/week2/helloSpring/src/main/java/hello/helloSpring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.helloSpring.repository;
+
+import hello.helloSpring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+//가장 단순하게 만듬 실무에서는 이것보다 복잡하게 사용.
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+    public void clearStore() {
+        store.clear();
+    }
+}
